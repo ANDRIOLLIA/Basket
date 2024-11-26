ab89e81b9e4489997b30f0b9489e6385fda6eb43
ANDRIOLLIA <andreymatosian94@gmail.com>
2024-11-26 12:48:06 +0300

    basket

---
+++ src/main/java/Main2.java
@@ -0,0 +1,7 @@
+//public class Main2 {
+//    public static void main(String[] args) {
+//        Basket firstBasket = new Basket();
+//        firstBasket.add("колбаса", 600);
+//        System.out.println(firstBasket.getTotalPrice());
+//    }
+//}
---
+++ src/main/java/Product.java
@@ -0,0 +1,44 @@
+public class Product {
+    private String name;
+    private int price;
+    private double weight;
+
+    public Product(String name, int price, double weight) {
+        this.name = name;
+        this.price = price;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", weight=" + weight +
+                '}';
+    }
+}
